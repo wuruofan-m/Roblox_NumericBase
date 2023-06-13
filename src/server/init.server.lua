9253b98d4b4a3a7a050f1db9640a7177cedf29c5
ruofan.wu <ruofan.wu@appshahe.com>
2023-06-13 12:14:14 +0800

    add:rojo实例化

---
+++ src/server/init.server.lua
@@ -0,0 +1,9 @@
+--[[
+Author: ruofan.wu ruofan.wu@appshahe.com
+Date: 2023-06-13 12:08:31
+LastEditors: ruofan.wu ruofan.wu@appshahe.com
+LastEditTime: 2023-06-13 12:13:26
+FilePath: \Roblox_NumericBase\src\server\init.server.lua
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+--]]
+print("Hello world, from server!!!!")
